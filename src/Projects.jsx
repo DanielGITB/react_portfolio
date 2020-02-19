f6075078af6b7bd35373f8572aa44ffef0eec125
DanielB <1996.daniel.bryant@gmail.com>
2020-02-19 15:01:54 +0100

    adding projectcard - description for presented projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,30 +1,43 @@
-import React, { Component } from "react";
 import Axios from "axios";
+import React, { Component } from "react";
+import ProjectCard from "./ProjectCard";
+
 
 class Projects extends Component {
     state = {
         projects:[]
     }
 
+
+    componentDidMount() {
+        axios.get('./src/data/projects.json')
+          .then(response => {
+            this.setState({
+              projects: response.data
+            })
+          })
+      }
+
     render() {
     const projects = this.state.projects;
     let projectsList;
 
-    if (projects.length > 0) {
-    projectList = projects.map(project => {
-        return (
-            <div id={'project-' + project.id} key={project.id}>
-                <h3 className="ui header">{project.name}</h3>
-            </div>
 
-        );
-    });
+
+    if (projects.length > 0) {
+        projectList = projects.map(project => {
+            return (
+                <div id={'project-' + project.id} key={project.id}>
+                    <ProjectCard project={project} />
+                </div>
+            )
+        })
     }
 
     return (
         <div className="ui main container">
             <h1 id=" projects-header" className="uiheader">My Projects</h1>
-            {projectsList}
+            <div className="ui stackable four column grid">{ProjectsList}</div>
         </div>
 
     )
@@ -34,4 +47,6 @@ class Projects extends Component {
 
 
 }
+
+
 export default Projects;
