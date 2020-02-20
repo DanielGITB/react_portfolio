78324f064f7fac6ca8ee6cdf091999c2a66dbf3f
DanielB <1996.daniel.bryant@gmail.com>
2020-02-20 13:17:29 +0100

    fixes bug with my projects page

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,63 +2,48 @@ import axios from "axios";
 import React, { Component } from "react";
 import ProjectCard from "./ProjectCard";
 import { UndrawDashboard } from "react-undraw-illustrations";
-
-
-
 class Projects extends Component {
-    state = {
-        projects:[]
-    }
-
-
-    componentDidMount() {
-        axios.get('./src/data/projects.json')
-          .then(response => {
-            this.setState({
-              projects: response.data
-            })
-          })
-    }
-
-    render() {
+  state = {
+    projects: []
+  }
+  componentDidMount() {
+    axios.get('./src/data/projects.json').then(response => {
+      this.setState({
+        projects: response.data
+      })
+    })
+  }
+  render() {
     const projects = this.state.projects;
     let projectsList;
-
-
-
     if (projects.length > 0) {
-        projectList = projects.map(project => {
-            return (
-                <div id={'project-' + project.id} key={project.id}>
-                    <ProjectCard project={project} />
-                </div>
-            )
-        })
-    }
-
-    return (
-        <div className="ui main container">
-          <div className="ui stackable two column grid">
-            <div className="column">
-              <UndrawDashboard primaryColor='#283a' height='200px' />
-            </div>
-            <div className="column">
-              <h1 className="ui header">My Projects</h1>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia quod
-                ab doloremque eaque. Consequatur temporibus, quos enim, eaque nemo ad
-                iusto sequi modi totam qui veniam? Ab asperiores inventore distinctio.
-              </p>
-            </div>
+      projectsList = projects.map(project => {
+          return (
+              <div id={'project-' + project.id} key={project.id}>
+                  <ProjectCard project={project} />
+              </div>
+          )
+      })
+  }
+  return (
+      <div className="ui main container">
+        <div className="ui stackable two column grid">
+          <div className="column">
+            <UndrawDashboard primaryColor='#283a' height='200px' />
           </div>
-          <div className="ui stackable four column grid">{projectsList}</div>
+          <div className="column">
+            <h1 className="ui header">My Projects</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia quod
+              ab doloremque eaque. Consequatur temporibus, quos enim, eaque nemo ad
+              iusto sequi modi totam qui veniam? Ab asperiores inventore distinctio.
+            </p>
+          </div>
         </div>
-      );
-
-    }
-
-
-
+        <div className="ui stackable four column grid">{projectsList}</div>
+      </div>
+    );
+  }
 }
 
 
